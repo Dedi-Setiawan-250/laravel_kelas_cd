e43d49bdf0badf0c9dc856f529871137c6855953
Dedi-Setiawan-250 <dedi.setiawan@ft-umt.ac.id>
2020-08-08 04:36:00 +0700

    Pertemuan 13 Crud

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,4 @@
 <!-- Image and text -->
 <nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="#"><img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30"
-    height="30" class="d-inline-block align-top" alt="" loading="lazy">Sistem Web Lanjutan</a>
+    <a class="navbar-brand" href="#"><img src="umt.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy"> Sistem Web Lanjutan</a>
  </nav>
