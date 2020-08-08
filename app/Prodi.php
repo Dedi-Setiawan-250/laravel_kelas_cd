eeb587dcaeb49e13165384034fe4c8d406270c76
Dedi-Setiawan-250 <dedi.setiawan@ft-umt.ac.id>
2020-08-08 16:54:40 +0700

    Pertemuan 14 CrudPordi

--- app/Prodi.php
+++ app/Prodi.php
@@ -9,9 +9,14 @@ class Prodi extends Model
     protected $table = 'prodi';
     protected $primaryKey = 'kode_prodi';
 
+
+    protected $fillable = ['kode_prodi','nama_prodi','kaprodi',''];
+
+    //Field yang diabaikan isiannya
+    protected $guarded = [];
+
     public function mahasiswa()
     {
-    return $this->belongsTo('App\Mahasiswa','prodi','kode_prodi');
+        return $this->belongsTo('App\Mahasiswa','prodi','kode_prodi');
     }
-
 }
